9f594df8fd753c3679dd8c78d5127cf17f45fce4
Erick <ericklfdev@gmail.com>
2025-03-24 21:25:04 -0300

    new features: Classes and methods

--- src/AppTelefonico.java
+++ src/AppTelefonico.java
@@ -1,3 +1,77 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class AppTelefonico {
-    // Deve ser feito em collections.map para que não haja duplicação de dados
+    private Map<String, String> contatos;
+
+    public AppTelefonico(){
+        contatos = new HashMap<>();
+    }
+
+    public void adicionarContatos(String nome, String telefone){
+        if (!contatos.containsKey(nome) && !contatos.containsKey(telefone)){
+            contatos.put(nome, telefone);
+            System.out.println("Contato adicionado: "+ nome + " - "+ telefone);
+        }else {
+            System.out.println("Contato não adicionado");
+        }
+    }
+
+    public void exibirContatos(){
+        System.out.println("Lista telefonica: ");
+        for (Map.Entry<String, String> entry : contatos.entrySet()){
+            System.out.println(entry.getKey()+ " - "+ entry.getValue());
+        }
+    }
+
+    public void removerContatos(String nome, String telefone){
+        contatos.remove(nome, telefone);
+    }
+
+    public String buscarTelefone(String telefone){
+        return contatos.get(telefone);
+    }
+
+    public void ligar(String telefone){
+        System.out.println("Selecione o contato: ");
+        exibirContatos();
+        String numero = buscarTelefone(telefone);
+        if (numero != null){
+            System.out.println("Ligando para: "+ numero);
+        }
+        else {
+            System.out.println("Contato não encontrado!");
+        }
+    }
+    public void atender(){
+        System.out.println("Chamando...");
+        System.out.println("-----------");
+        System.out.println("Ligaçãoa atendida!");
+    }
+
+    public void timer(){
+        final Integer[] segundos = {1};
+        new Thread(() -> {
+            do {
+                try {
+                    Thread.sleep(1000);
+                    System.out.println(segundos[0]+ "...");
+                    segundos[0]++;
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }while (segundos[0] < 30);
+
+
+        });
+    }
+
+    public void iniciarCorreioVoz(){
+        System.out.println("iniciando correio de voz");
+        timer();
+        System.out.println("Finalizando correio de voz");
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        AppMusical appMusical = new AppMusical();
+        AppTelefonico appTelefonico = new AppTelefonico();
+        AppWeb appWeb = new AppWeb();
+
+        System.out.println("Iniciando app musical");
+
+        appMusical.adicionarMusica(lista ->{
+            lista.add("Shake it off");
+            lista.add("Dive");
+            lista.add("Master of Puppets");
+            lista.add("T.N.T");
+        });
+        appMusical.exibirMusicas();
+        appMusical.tocarMusica("Shake it off");
+        appMusical.proximaMusica();
+        appMusical.pausarMusica("shake it off");
+        appMusical.removerMusicas("Dive");
+        appMusical.exibirMusicas();
+
+        System.out.println("==============fim===============");
+        System.out.println("Iniciando app web");
+
+        appWeb.setUrl("www.youtube.com");
+        appWeb.exibirPag(appWeb.getUrl());
+        appWeb.atualizarPag();
+        appWeb.novaPag("www.facebook.com");
+        System.out.println("=========fim==============");
+        System.out.println("Iniciando app telefonico:");
+        appTelefonico.adicionarContatos("Arnaldo", "1124258798");
+        appTelefonico.adicionarContatos("Maria", "1124258798");
+        appTelefonico.adicionarContatos("Jose", "1124657321");
+        appTelefonico.adicionarContatos("Arnaldo", "1131449875");
+
+        appTelefonico.exibirContatos();
+
+        appTelefonico.ligar("1131449875");
+        appTelefonico.atender();
+        appTelefonico.iniciarCorreioVoz();
+        appTelefonico.removerContatos("Jose", "1124657321");
+
+        System.out.println("Lista após remoção");
+        appTelefonico.exibirContatos();
+
     }
+
 }
